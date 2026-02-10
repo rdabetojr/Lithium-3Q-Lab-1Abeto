978b264b244e0ac2ab0a30d7a56a3757ac8088fc
rdabetojr <rdabeto29@src.pshs.edu.ph>
2026-02-10 08:18:45 +0800

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const PI = 3.14;
 const G = 6.6743*10**-11
 
-function getVolumeOfCone() {
+function getOrbitPeriod() {
     let radius = parseFloat(document.getElementById("input-radius").value);
     let mass = parseFloat(document.getElementById("input-mass").value);
-    let volume = Math.sqrt((4*PI**2)*radius**3/G*mass);
+    let period = Math.sqrt((4 * PI ** 2 * radius ** 3)/(G*mass));
 
-    let output = `The volume is ${volume}`;
-    document.getElementById("result-volume").innerText = output;
+    let output = `The period/time is ${period}`;
+    document.getElementById("result-period").innerText = output;
 }
